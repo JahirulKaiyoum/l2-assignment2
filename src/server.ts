3cffe4ac8843e86005055f519df4c415caccb0e7
JahirulKaiyoum <52067522+JahirulKaiyoum@users.noreply.github.com>
2023-11-24 15:28:48 +0600

    project setup done

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import config from './App/config';
+import app from './app';
+
+import mongoose from 'mongoose';
+
+// main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(config.database_url as string);
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`);
+  });
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+main();
